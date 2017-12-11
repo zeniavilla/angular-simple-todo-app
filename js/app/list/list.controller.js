7fad7774a8a27b96c6697f38a4b9b090473d590e
Zenia <zeniavilla@hotmail.com>
2017-12-11 10:27:43 -0800

    Create ListController with list.tasks

---
+++ js/app/list/list.controller.js
@@ -0,0 +1,32 @@
+angular
+  .module("todoApp")
+  .controller("ListController", ListController);
+
+function ListController() {
+  var vm = this;
+  vm.list = {
+    name: 'Todo List',
+    tasks: [
+      {
+        id: 1,
+        name: 'Take out trash',
+        complete: false
+      },
+      {
+        id: 2,
+        name: 'Buy Grandma a present',
+        complete: false
+      },
+      {
+        id: 3,
+        name: 'Wash car',
+        complete: true
+      },
+      {
+        id: 4,
+        name: 'Fix bathroom sink',
+        complete: false
+      }
+    ]
+  }
+}
